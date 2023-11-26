b0f53ef37f302afccce0e665158b77f4e545d5fa
Abhay Kumar Srivastava <ms2364911@gmail.com>
2023-11-26 20:08:28 +0530

    config

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const app = require("./app");
 const dotenv = require("dotenv");
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:3001'); // Replace with your actual frontend origin
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Origin', '*'); 
+  next();
+});
+
 
 const connectDataBase = require("./databse/database");
 const { v2 } = require("cloudinary");
