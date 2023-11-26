5966f8c9684493c1311d2d34f2de07cd5f3366d6
Abhay Kumar Srivastava <ms2364911@gmail.com>
2023-11-26 20:36:19 +0530

    fixed route

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const app = express();
+const app = require('./app')
 const dotenv = require("dotenv");
 
 const connectDataBase = require("./databse/database");
@@ -11,26 +11,17 @@ v2.config({
   api_secret: process.env.API_SECRET,
 });
 
-// CORS Middleware
+
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:3001');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true'); // Add this line
+  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:3001");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true"); // Add this line
   next();
 });
 
-// Other Middleware and Route Configurations
-app.use(express.json({ limit: "100mb" }));
-app.use(express.urlencoded({ limit: "100mb", extended: true }));
-
-// Other routes or middleware if needed
-// ...
-
-// Database Connection
 connectDataBase();
 
-// Start the Server
 const PORT = process.env.PORT || 4001;
 app.listen(PORT, () => {
   console.warn(`Server is running on port ${PORT}`);
