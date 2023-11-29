c8f66080433ac7a88de6a229405cb8b5ec39c580
Abhay Kumar Srivastava <ms2364911@gmail.com>
2023-11-29 17:56:17 +0530

    cors modid=fied

--- app.js
+++ app.js
@@ -6,12 +6,7 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const app = express();
 
-app.use(
-  cors({
-    credentials: true,
-    origin: "http://localhost:3000",
-  })
-);
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
